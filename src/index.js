a909ba09f11f354cdbd3b976ab44ce452b689b16
Mariia_Ostapets <mariyaostapets@gmail.com>
2025-05-29 13:24:02 +0300

    Add basic folders, error handler middlewares, move setupServer into server.js

--- src/index.js
+++ src/index.js
@@ -1,42 +1,7 @@
-import express from 'express';
-import pino from 'pino-http';
-import cors from 'cors';
+import { setupServer } from './server.js';
 
-const app = express();
+const bootstrap = async () => {
+  setupServer();
+};
 
-const PORT = 3000;
-
-app.use(express.json());
-
-app.use(cors());
-
-app.use(
-  pino({
-    transport: {
-      target: 'pino-pretty',
-    },
-  }),
-);
-
-app.get('/', (req, res) => {
-  res.json({
-    message: 'Welcome to the E-Pharmacy API!',
-  });
-});
-
-app.use((req, res, next) => {
-  res.status(404).json({
-    message: 'Not found',
-  });
-});
-
-app.use((err, req, res, next) => {
-  res.status(500).json({
-    message: 'Something went wrong',
-    error: err.message,
-  });
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+bootstrap();
