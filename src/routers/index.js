5d088dcba52dea05fad53693adf15e75d93e8fd5
Mariia_Ostapets <mariyaostapets@gmail.com>
2025-06-30 13:40:18 +0300

    Create product route

--- src/routers/index.js
+++ src/routers/index.js
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import authRouter from './auth.js';
 import storesRouter from './store.js';
 import reviewsRouter from './review.js';
+import productsRouter from './product.js';
 
 const router = Router();
 
 router.use('/user', authRouter);
 router.use('/stores', storesRouter);
 router.use('/customer-reviews', reviewsRouter);
+router.use('/products', productsRouter);
 
 export default router;
